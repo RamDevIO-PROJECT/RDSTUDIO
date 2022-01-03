2f233bfa5cb63a06123541543d1edc1e0ceb3a92
RamDevIO1 <ramdev.io.id@gmail.com>
2022-01-03 16:18:14 +0700

    a

---
+++ public/script.js
@@ -0,0 +1,70 @@
+const videoUrl = document.querySelector('.video-url-input')
+const startBtn = document.querySelector('.start-btn')
+const downloadBtn = document.querySelector('.download-btn')
+const videoThumbnail = document.querySelector('.video-thumbnail')
+const videoTitle = document.querySelector('.video-title')
+const ResolutionsList = document.querySelector('.resolutions-list')
+
+
+const getVideoID = () => {
+  const url = videoUrl.value
+  const searchParams = new URLSearchParams(url.split('?')[1])
+  return searchParams.get('v')
+}
+
+const getVideoInfo = async (id) => {
+  const res = await fetch(`/api/video?id=${id}`)
+  return res.json()
+}
+
+const showResolutions = (resolutions) => {
+  const html = resolutions
+  .map((resolution, i) => `
+    <label>
+      <input type="radio" name="resolution" value="${resolution}" ${i === 0 ? 'checked' : ''}>
+      ${resolution}
+    </label>
+  `)
+  .join('')
+  
+  ResolutionsList.innerHTML = html
+}
+
+const getDownloadActhor = ({id, resolution, format}) => {
+  let url = `/download?id=${id}&format=${format}`
+  if (format === 'video') {
+    url += `&resolution=${resolution}`
+  }
+  const a = document.createElement('a')
+  a.herf = url
+  a.download = true
+  
+  return a
+}
+
+const download = ({id, resolution, format}) => {
+  const link = getDownloadActhor({id, resolution, format})
+  link.click()
+}
+
+const getRadioValue = (name) => {
+  document.querySelector(`[name="${name}"]:checked`).valur
+}
+
+startBtn.addEventListener('click', async () => {
+  const id = getVideoID()
+  const { title, resolutions, thumbnailURL } = await getVideoInfo(id)
+  
+  videoThumbnail.src = thumbnailURL
+  videoTitle.textContent = title
+  showResolutions(resolutions)
+})
+
+downloadBtn.addEventListener('click', () => {
+  download({
+    id: getVideoID(),
+    resolution: getRadioValue('resolutions'),
+    format: getRadioValue('format')
+  })
+})
+
